c7b994da13ea92683643b0f9dddf1fda159494e0
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-13 21:21:31 -0300

    HealthcareProviders Actions have been created.

---
+++ Sejmet.API/Commands/HealthcareProviders/Create/Handler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sejmet.API.Repositories.Interfaces;
+
+namespace Sejmet.API.Commands.HealthcareProviders.Create
+{
+    public class Handler : IRequestHandler<CommandRequest, IActionResult>
+    {
+        private readonly IHealthcareProvidersRepository _healthcareProvidersRepository;
+
+        public Handler(IHealthcareProvidersRepository healthcareProvidersRepository)
+        {
+            _healthcareProvidersRepository = healthcareProvidersRepository;
+        }
+
+        public async Task<IActionResult> Handle(CommandRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var createdHealthcareProvider = await _healthcareProvidersRepository.CreateHealthcareProviderAsync(request.Body, cancellationToken);
+                return new CreatedAtRouteResult(createdHealthcareProvider.Id, new { createdHealthcareProvider.Id, Name = createdHealthcareProvider.Name, IsActive = createdHealthcareProvider.IsActive});
+            }
+            catch (Exception e)
+            {
+                return new ObjectResult(new ProblemDetails() { Detail = e.Message }) { StatusCode = 500 };
+            }
+            
+        }
+    }
+}
---
+++ Sejmet.API/Commands/HealthcareProviders/Patch/ValidatorCollection.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Sejmet.API.Commands.HealthcareProviders.Patch
+{
+    public class ValidatorCollection : AbstractValidator<CommandRequest>
+    {
+        public ValidatorCollection() 
+        {
+            this.ClassLevelCascadeMode = CascadeMode.Stop;
+
+            this.RuleFor(x => x.ProviderID)
+                .NotEmpty();
+        }
+    }
+}
