8d0eca752941cd3a2681c14f3e2c070a7a59968e
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-13 21:24:21 -0300

    HealthcareProvider Mapping Profile has been added.

---
+++ Sejmet.API/Models/Mappings/HealthcareProviderProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Sejmet.API.Models.DTOs;
+using Sejmet.API.Models.Entities;
+
+namespace Sejmet.API.Models.Mappings
+{
+    public class HealthcareProviderProfile : Profile
+    {
+        public HealthcareProviderProfile()
+        {
+            this.CreateMap<HealthcareProviderDTO, HealthcareProvider>()
+                .ForMember(destination => destination.Name, origin => origin.MapFrom(x => x.Name))
+                .ForMember(destination => destination.IsActive, origin => origin.MapFrom(x => x.IsActive))
+                .ForMember(destination => destination.Id, origin => origin.MapFrom(x => x.Id))
+                .ReverseMap();
+        }
+    }
+}
