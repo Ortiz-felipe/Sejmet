7061647620d8e69b45eb331a8b0796476f27b211
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-14 18:13:42 -0300

    Moved all the DTOs to separate folders

---
+++ Sejmet.API/Models/DTOs/Customers/UpdateCustomerDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Sejmet.API.Models.DTOs.Customers
+{
+    public class UpdateCustomerDTO
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public Guid HealthCareProviderId { get; set; }
+        public Guid CityId { get; set; }
+        public int ProvinceId { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ Sejmet.API/Models/DTOs/HealthcareProvider/HealthcareProviderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Sejmet.API.Models.DTOs.HealthcareProvider
+{
+    public class HealthcareProviderDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
