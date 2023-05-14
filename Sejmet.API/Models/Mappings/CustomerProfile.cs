ab1585025b678fefc2dd14baf8ee3dcffb6911b6
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-13 21:03:23 -0300

    Customer & UpdateCustomer Mapping Profiles have been added.

---
+++ Sejmet.API/Models/Mappings/CustomerProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Sejmet.API.Models.DTOs;
+using Sejmet.API.Models.Entities;
+
+namespace Sejmet.API.Models.Mappings
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile() 
+        {
+            this.CreateMap<Customer, CreateProductDTO>()
+                .ForMember(destination => destination.Id, origin => origin.MapFrom(x => x.Id))
+                .ForMember(destination => destination.FirstName, origin => origin.MapFrom(x => x.FirstName))
+                .ForMember(destination => destination.LastName, origin => origin.MapFrom(x => x.LastName))
+                .ForMember(destination => destination.Birthdate, origin => origin.MapFrom(x => x.Birthdate))
+                .ForMember(destination => destination.Age, origin => origin.MapFrom(x => x.Age))
+                .ForMember(destination => destination.HealthCareProviderId, origin => origin.MapFrom(x => x.HealtcareProviderId))
+                .ForMember(destination => destination.PhoneNumber, origin => origin.MapFrom(x => x.PhoneNumber))
+                .ForMember(destination => destination.CityId, origin => origin.MapFrom(x => x.CityId))
+                .ForMember(destination => destination.ProvinceId, origin => origin.MapFrom(x => x.ProvinceId));
+        }
+    }
+}
---
+++ Sejmet.API/Models/Mappings/UpdateCustomerProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Sejmet.API.Models.DTOs;
+using Sejmet.API.Models.Entities;
+
+namespace Sejmet.API.Models.Mappings
+{
+    public class UpdateCustomerProfile : Profile
+    {
+        public UpdateCustomerProfile() 
+        {
+            this.CreateMap<UpdateCustomerDTO, Customer>()
+                .ForMember(dest => dest.Id, origin => origin.MapFrom(x => x.Id))
+                .ForMember(dest => dest.FirstName, origin => origin.MapFrom(x => x.FirstName))
+                .ForMember(dest => dest.LastName, origin => origin.MapFrom(x => x.LastName))
+                .ForMember(dest => dest.Address, origin => origin.MapFrom(x => x.Address))
+                .ForMember(dest => dest.CityId, origin => origin.MapFrom(x => x.CityId))
+                .ForMember(dest => dest.ProvinceId, origin => origin.MapFrom(x => x.ProvinceId))
+                .ForMember(dest => dest.PhoneNumber, origin => origin.MapFrom(x => x.PhoneNumber))
+                .ForMember(dest => dest.HealtcareProviderId, origin => origin.MapFrom(x => x.HealthCareProviderId));
+        }
+    }
+}
