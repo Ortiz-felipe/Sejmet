03cbd749b5a09744523635f0c71fb4923315c187
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-13 21:01:07 -0300

    CustomerDTO & UpdateCustomerDTO have been added.

---
+++ Sejmet.API/Models/DTOs/CustomerDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Sejmet.API.Models.DTOs
+{
+    public class CustomerDTO
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Address { get; set; } = String.Empty;
+        public DateTime Birthdate { get; set; }
+        public int Age { get; set; }
+        public Guid HealthCareProviderId { get; set; }
+        public Guid CityId { get; set; }
+        public int ProvinceId { get; set; }
+        public string PhoneNumber { get; set; } = String.Empty;
+
+    }
+}
---
+++ Sejmet.API/Models/DTOs/UpdateCustomerDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Sejmet.API.Models.DTOs
+{
+    public class UpdateCustomerDTO
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Address { get; set; } = String.Empty;
+        public Guid HealthCareProviderId { get; set; }
+        public Guid CityId { get; set; }
+        public int ProvinceId { get; set; }
+        public string PhoneNumber { get; set; } = String.Empty;
+    }
+}
