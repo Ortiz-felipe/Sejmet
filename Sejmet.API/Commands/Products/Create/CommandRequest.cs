f8f7c05da3e958094cb39efa216b9f85bba289e5
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-14 18:24:09 -0300

    Create Product, Get All & GetProductByProductCode actions have been implemented.

---
+++ Sejmet.API/Commands/Products/Create/CommandRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sejmet.API.Models.DTOs.Products;
+
+namespace Sejmet.API.Commands.Products.Create
+{
+    public class CommandRequest : IRequest<IActionResult>
+    {
+        [FromBody]
+        public CreateProductDTO Body { get; set; }
+    }
+}
---
+++ Sejmet.API/Queries/Products/GetAll/Handler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sejmet.API.Repositories.Interfaces;
+
+namespace Sejmet.API.Queries.Products.GetAll
+{
+    public class Handler : IRequestHandler<QueryRequest, IActionResult>
+    {
+        private readonly IProductsRepository _productsRepository;
+
+        public Handler(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+
+        public async Task<IActionResult> Handle(QueryRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var products = await _productsRepository.GetProductsAsync(request.SearchExpression, cancellationToken).ConfigureAwait(false);
+
+                return new OkObjectResult(products);
+            }
+            catch (Exception e)
+            {
+                return new ObjectResult(new ProblemDetails() { Detail = e.Message }) { StatusCode = 500 };
+            }
+            
+        }
+    }
+}
