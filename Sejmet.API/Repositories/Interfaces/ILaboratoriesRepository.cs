393c4ebfc301a4edfbd533d2f30a38c89a6407c1
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-20 11:23:28 -0300

    Laboratories Interface and Repo have been created

---
+++ Sejmet.API/Repositories/Interfaces/ILaboratoriesRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sejmet.API.Models.DTOs.Laboratories;
+
+namespace Sejmet.API.Repositories.Interfaces
+{
+    public interface ILaboratoriesRepository
+    {
+        Task<IList<LaboratoryDTO>> GetAllLaboratoriesAsync(string searchExpression, CancellationToken cancellationToken);
+        Task<CreateLaboratoryDTO> CreateLaboratoryAsync(CreateLaboratoryDTO createLaboratoryDTO, CancellationToken cancellationToken);
+    }
+}
---
+++ Sejmet.API/Repositories/LaboratoriesRepository.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Sejmet.API.Models.DTOs.Laboratories;
+using Sejmet.API.Models.Entities;
+using Sejmet.API.Repositories.Interfaces;
+
+namespace Sejmet.API.Repositories
+{
+    public class LaboratoriesRepository : ILaboratoriesRepository
+    {
+        private readonly SejmetDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public LaboratoriesRepository(SejmetDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public async Task<CreateLaboratoryDTO> CreateLaboratoryAsync(CreateLaboratoryDTO createLaboratoryDTO, CancellationToken cancellationToken)
+        {
+            var laboratory = _mapper.Map<Laboratory>(createLaboratoryDTO);
+
+            try
+            {
+                await _dbContext.AddAsync(laboratory, cancellationToken);
+                await _dbContext.SaveChangesAsync(cancellationToken);
+
+                return _mapper.Map<CreateLaboratoryDTO>(laboratory);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IList<LaboratoryDTO>> GetAllLaboratoriesAsync(string searchExpression, CancellationToken cancellationToken)
+        {
+            var query = _dbContext.Laboratories.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(searchExpression))
+            {
+                query = query.Where(x => x.Name.Contains(searchExpression));
+            }
+
+            try
+            {
+                var laboratories = query.ToListAsync(cancellationToken);
+
+                return _mapper.Map<List<LaboratoryDTO>>(laboratories);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
