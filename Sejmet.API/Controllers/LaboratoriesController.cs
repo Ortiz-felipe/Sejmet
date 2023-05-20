4651b0ce5271436dbe2217227fcdeec1983a5b99
Felipe Ortiz Correa <ortiz.jfelipe@gmail.com>
2023-05-20 11:24:46 -0300

    Laboratories Controller has been created

---
+++ Sejmet.API/Controllers/LaboratoriesController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sejmet.API.Queries.Laboratories.GetAll;
+
+namespace Sejmet.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LaboratoriesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LaboratoriesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllLaboratoriesAsync(QueryRequest queryRequest)
+        {
+            return await _mediator.Send(queryRequest);
+        }
+    }
+}
