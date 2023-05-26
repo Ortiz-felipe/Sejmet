ee511b343dcf49905205d4de867b7bf561333f60
Felipe Ortiz <ortiz.jfelipe@gmail.com>
2023-05-26 12:09:25 -0300

    Transaction Status Enum has been created.

---
+++ Sejmet.API/Repositories/TransactionStatusesEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Sejmet.API.Repositories
+{
+    public enum TransactionStatusesEnum
+    {
+        InProgress = 1,
+        Accepted = 2,
+        InTransit = 3,
+        Recieved = 4,
+        Cancelled = 5,
+    }
+}
