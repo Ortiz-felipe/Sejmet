36e55051ecd0905bb4eebb4d895b42da96fcdb10
Felipe Ortiz <ortiz.jfelipe@gmail.com>
2023-05-26 12:45:33 -0300

    Corrected a typo

--- Sejmet.API/Models/Mappings/ProductProfile.cs
+++ Sejmet.API/Models/Mappings/ProductProfile.cs
@@ -10,7 +10,7 @@ namespace Sejmet.API.Models.Mappings
         {
             this.CreateMap<Product, ProductDTO>()
                 .ForMember(destination => destination.Upc, origin => origin.MapFrom(x => x.Upc))
-                .ForMember(destiantion => destiantion.TradeName, origin => origin.MapFrom(x => x.TradeName))
+                .ForMember(destination => destination.TradeName, origin => origin.MapFrom(x => x.TradeName))
                 .ForMember(destination => destination.LaboratoryName, origin => origin.MapFrom(x => x.Laboratory.Name))
                 .ForMember(destination => destination.DosageFormName, origin => origin.MapFrom(x => x.DosageForm.Name))
                 .ForMember(destination => destination.ActiveCompoundName, origin => origin.MapFrom(x => x.ActiveCompound.Name))
