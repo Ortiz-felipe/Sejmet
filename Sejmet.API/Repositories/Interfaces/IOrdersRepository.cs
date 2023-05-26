4b8b5b006f8981c69c9c4bc8af88f8bd7bb76a47
Felipe Ortiz <ortiz.jfelipe@gmail.com>
2023-05-26 12:08:35 -0300

    Order Repository & it's Interface have been created.

---
+++ Sejmet.API/Repositories/Interfaces/IOrdersRepository.cs
@@ -0,0 +1,12 @@
+﻿using Sejmet.API.Models.DTOs.Orders;
+
+namespace Sejmet.API.Repositories.Interfaces
+{
+    public interface IOrdersRepository
+    {
+        Task<IList<OrderDTO>> GetAllOrdersAsync(string providerName, CancellationToken cancellationToken);
+        Task<OrderDTO?> GetOrderByIdAsync(Guid orderId, CancellationToken cancellationToken);
+        Task<CreateOrderDTO> CreateOrderAsync(CreateOrderDTO orderDTO, CancellationToken cancellationToken);
+        Task<OrderDTO?> UpdateOrderAsync(UpdateOrderDTO orderDTO, CancellationToken cancellationToken);
+    }
+}
---
+++ Sejmet.API/Repositories/OrdersRepository.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Sejmet.API.Models.DTOs.Orders;
+using Sejmet.API.Models.Entities;
+using Sejmet.API.Repositories.Interfaces;
+
+namespace Sejmet.API.Repositories
+{
+    public class OrdersRepository : IOrdersRepository
+    {
+        private readonly SejmetDbContext _context;
+        private readonly IMapper _mapper;
+
+        public OrdersRepository(SejmetDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IList<OrderDTO>> GetAllOrdersAsync(string providerName, CancellationToken cancellationToken)
+        {
+            var query = _context.Orders
+                                            .Include(x => x.Provider)
+                                            .Include(x => x.OrderProducts)
+                                            .ThenInclude(x => x.Product).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(providerName))
+            {
+                query = query.Where(x => x.Provider.Name.Contains(providerName));
+            }
+
+            var orders = await query.ToListAsync(cancellationToken);
+
+            return _mapper.Map<List<OrderDTO>>(orders);
+        }
+
+        public async Task<OrderDTO?> GetOrderByIdAsync(Guid orderId, CancellationToken cancellationToken)
+        {
+            var order = await _context.Orders
+                                    .Include(x => x.Provider)
+                                    .Include(x => x.OrderProducts)
+                                    .ThenInclude(x => x.Product)
+                                    .FirstOrDefaultAsync(x => x.Id == orderId, cancellationToken);
+
+            return order is null ? null : _mapper.Map<OrderDTO>(order);
+        }
+
+        public async Task<CreateOrderDTO> CreateOrderAsync(CreateOrderDTO orderDTO, CancellationToken cancellationToken)
+        {
+            var newOrder = _mapper.Map<Order>(orderDTO);
+
+            await _context.AddAsync(newOrder, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<CreateOrderDTO>(newOrder);
+        }
+
+        public async Task<OrderDTO?> UpdateOrderAsync(UpdateOrderDTO orderDTO, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await _context.Orders
+                .Include(x => x.Provider)
+                .Include(x => x.OrderProducts)
+                .ThenInclude(x => x.Product)
+                .FirstOrDefaultAsync(x => x.Id == orderDTO.OrderId, cancellationToken);
+
+            if (orderToUpdate is null)
+            {
+                return null;
+            }
+
+            orderToUpdate.TransactionStatusId = orderDTO.TransactionStatusId;
+            orderToUpdate.RecievedDate = DateTime.UtcNow;
+
+            if (orderDTO.TransactionStatusId == (int)TransactionStatusesEnum.Recieved)
+            {
+                foreach (var product in orderToUpdate.OrderProducts)
+                {
+                    var updatedProduct = await _context.Products.FirstOrDefaultAsync(x => x.Id == product.ProductId, cancellationToken);
+
+                    if (updatedProduct != null)
+                    {
+                        updatedProduct.CurrentStock += product.Quantity;
+                    }
+                }
+            }
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<OrderDTO>(orderToUpdate);
+
+        }
+    }
+}
