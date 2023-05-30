c45f4d8ad9af24c3bc0e5f7d79ff7bf245a60b3c
Damian Triebl <damian.triebl@gmail.com>
2023-05-30 20:20:44 -0300

    refactoring table

--- Frontend/src/pages/home/Home.tsx
+++ Frontend/src/pages/home/Home.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable prettier/prettier */
-import Card from "../ui/Card/Card"
+import Card from "../../ui/Card/Card"
 import { StyledHome } from "./StyledHome"
-
+import MedsCard from "./MedsCard"
 // this home is whit 2 squares and 1 rectangle
 const Home = () => {
     return (
         <StyledHome>
             <div className="home">
-                <Card title="Medicamentos mas vendidos"> Lista de medicamentos </Card>
+                <MedsCard />
                 <Card title="Medicamentos por acabarse" > otra cosa </Card>
                 <Card title="Promedio de ventas">una cosa</Card>
                 <Card title="Ultimos Pedidos Realizados" >una cosa</Card>
