e113bcc0624d3ab6144ad42465b35c936a3edbc0
Damian Triebl <damian.triebl@gmail.com>
2023-06-04 20:34:40 -0300

    agregado el reducer a la tabla de inventarios

--- Frontend/src/features/carro/carroSlice.ts
+++ Frontend/src/features/carro/carroSlice.ts
@@ -20,17 +20,33 @@ export const OrderSlice = createSlice({
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    addElement: (state, action: PayloadAction<OrderProduct>) => {   
-      console.log('payload', action)   
-      state.providerId +=  action.payload.productId
+    addElement: (state, action: PayloadAction<Orders>) => {   
+      console.log('payload', action.payload)   
+      state.orderedProducts =  [...state.orderedProducts, action.payload]
+      console.log('state', state.orderedProducts)  
     },
-    removeElement: (state, {payload}) => {
-      state.orderedProducts = state.orderedProducts.filter((item) => item.productId !== payload.productId)
+    addAllElements: (state, action: PayloadAction<Orders[]>) => {   
+      console.log('payload', action.payload)   
+      state.orderedProducts =  [...state.orderedProducts, ...action.payload]
+      console.log('state', state.orderedProducts)  
+    },
+    removeElement: (state, action: PayloadAction<Orders>) => {
+      console.log('payload pero para borrar', action.payload)   
+      console.log('state borrado', state.orderedProducts)   
+      state.orderedProducts = state.orderedProducts.filter((item) => item.upc !== action.payload.upc)
     },  
+    removeAllElement: (state, action: PayloadAction<Orders[]>) => {
+      console.log('payload pero para borrar', action.payload)   
+      console.log('state borrado', state.orderedProducts)   
+      state.orderedProducts = state.orderedProducts.filter((item) =>  !action.payload.some((item2) => item.upc === item2.upc))
+    }, 
   },
 
 })
 
-export const { addElement, removeElement } = OrderSlice.actions
-export const selectCarro = (state: RootState) => state.orders
+export const { addElement, addAllElements, removeAllElement, removeElement } = OrderSlice.actions
+export const selectCarro = (state: RootState) => state.orders.orderedProducts
+export const selectCarroCantidad = (state: RootState) => state.orders.orderedProducts.length
+export const selectCarroSelected = (state: RootState) => state.orders.orderedProducts.map((item) => item.upc)
+
 export default OrderSlice.reducer
